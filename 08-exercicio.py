01d49a33161c003db0e85dd8a4289c6a34a09b70
henriquemene <henriquemene7@gmail.com>
2022-09-12 15:05:22 -0300

    feito exercicio 8

---
+++ 08-exercicio.py
@@ -0,0 +1,5 @@
+print("-------desafio 08-------")
+hora =int(input("quanto voce ganha por hora trabalhada ?"))
+quantas_horas =int(input("quantas horas trabalhadas voce tem no mes ?"))
+mes = hora * quantas_horas
+print(f"voce ganha extamente R${mes} no més! ")
