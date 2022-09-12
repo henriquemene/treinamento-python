b9c7157d0cb334e0fe80ef6ce278787b0006545b
henriquemene <henriquemene7@gmail.com>
2022-09-12 16:17:17 -0300

    feito exercicio 13

---
+++ 13-exercicio.py
@@ -0,0 +1,6 @@
+print("-------desafio 13-------")
+altura = float(input("qual é a sua altura ? "))
+peso_homem = (72.7*altura)-58
+peso_mulher = (72.7*altura)-44.7
+print(f"o seu peso ideal para homem  é {peso_homem}")
+print(f"o peso ideal para mulher é {peso_mulher}")
