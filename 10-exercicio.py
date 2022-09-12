576945f2550130bf15d4ff07ce24e10d139ca3ab
henriquemene <henriquemene7@gmail.com>
2022-09-12 15:44:05 -0300

    feito exercicio 10

---
+++ 10-exercicio.py
@@ -0,0 +1,5 @@
+print("-------desafio 10-------")
+celsius = float(input("me informe a temperatura em celsius, que voce deseja converter para fahrenheit ? "))
+calculo = celsius + 32
+fahrenheit = calculo * 1.8
+print(f"a temperatura de {celsius}C coresponde a {fahrenheit}F")
