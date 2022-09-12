e4f6c30be81426d33bc0b3bb5d83943a820c83ca
henriquemene <henriquemene7@gmail.com>
2022-09-12 14:57:14 -0300

    feito o exercicio 7

--- 06-exercicio.py
+++ 06-exercicio.py
@@ -4,4 +4,4 @@ from cmath import pi
 print("-------desafio 06-------")
 raio = int(input("qual o raio do seu circulo ?"))
 area = (raio * raio)*3,14
-print("a area do seu circulo é" ,{area})
+print(f"a area do seu circulo é {area}")
---
+++ 07-exercicio.py
@@ -0,0 +1,4 @@
+print("-------desafio 07-------")
+area =int(input("qual é a area do quadrado ? "))
+dobro = area*2
+print(f"o dobro da area do seu quadrado é {dobro}")
