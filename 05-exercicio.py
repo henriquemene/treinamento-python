3276e61c5e27da58ba16563230ce5468ad730d0f
henriquemene <henriquemene7@gmail.com>
2022-09-12 14:13:38 -0300

    adicionando maus um arquivo

---
+++ 05-exercicio.py
@@ -0,0 +1,4 @@
+print("-------desafio 05-------")
+metros =int(input("digite a quantidade de metros que voce deseja ? "))
+centimetros = metros * 100
+print("com {} voce tem {} centimetros".format(metros,centimetros))
