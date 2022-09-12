e0433be3f725e309839d449c5c2060b81721d96f
henriquemene <henriquemene7@gmail.com>
2022-09-12 16:04:34 -0300

    feito exercicio 11

---
+++ 11-exercicio.py
@@ -0,0 +1,13 @@
+print("-------desafio 11-------")
+numero1=int(input("escolha um numero inteiro ? "))
+numero2 =int(input("escoha mais um numero inteiro ? "))
+numero_real =float(input("agora escolha um numero real"))
+
+resposta1= (numero1 *2)+(numero2 // 2)
+print(f"o dobro do primeiro e metade do segundo é {resposta1}")
+
+resposta2= (numero1 *3)+numero_real
+print(f"o triplo do primeiro com a soma do terceiro é {resposta2}")
+
+resposta3= (numero_real * numero_real)*3
+print(f"o terceiro elevado ao cubo é {resposta3}")
