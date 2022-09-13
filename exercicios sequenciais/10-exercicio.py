56cbd71af5052f0ef825024012775ebb4396e638
henriquemene <henriquemene7@gmail.com>
2022-09-13 12:43:52 -0300

    feito exercicio 10

---
+++ exercicios sequenciais/10-exercicio.py
@@ -0,0 +1,12 @@
+print("-------exercicio 10-------")
+print("reposnda com ")
+
+print("M-matutino  V-vespertino  N-norturno ")
+turno=str(input("em que turno voce estuda ? "))
+
+if turno == "M":
+    print("bom dia !")
+if turno == "V":
+        print("boa tarde !")
+if turno == "N":
+            print("boa noite!")
