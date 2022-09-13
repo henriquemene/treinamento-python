f2a69cc382afeb540c3db6b00c419f20f71d46c3
henriquemene <henriquemene7@gmail.com>
2022-09-13 12:36:07 -0300

    feito exercicio 9

---
+++ exercicios sequenciais/09-exercicio.py
@@ -0,0 +1,7 @@
+print("-------exercicio 9-------")
+numero1=int(input("escolha um numero ? "))
+numero2=int(input("escolha outro numero ? "))
+numero3=int(input("escolha outro numero ? "))
+lista = [numero1,numero2,numero3]
+lista.sort(reverse = True)
+print(lista)
