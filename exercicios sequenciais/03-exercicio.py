3af85c3bbf015e8c5dd0fe3bee2c884cefd6a70e
henriquemene <henriquemene7@gmail.com>
2022-09-13 10:11:54 -0300

    feito exercicio 3

---
+++ exercicios sequenciais/03-exercicio.py
@@ -0,0 +1,11 @@
+print("-------exercicio 3-------")
+letra=str(input("digite uma letra entre F ou M ? "))
+
+if letra == "F":
+    print("femenino")
+if letra == "M":
+    print("masculino")
+else:
+    print("sexo invalido")
+    
+   
