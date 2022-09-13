06145b212c72e10532ccd3b488d090ac16c9fc37
henriquemene <henriquemene7@gmail.com>
2022-09-13 09:49:56 -0300

    feito exercicio 2

---
+++ exercicios sequenciais/02-exercicio.py
@@ -0,0 +1,10 @@
+import string
+
+
+print("-------exercicio 2-------")
+valor=int(input("digite um valor ? "))
+
+if valor > 0:
+    print(f"o valor que voce digitou é positivo {valor}!")
+else:
+    print(f"o valor que voce digitou e negativo {valor}")
