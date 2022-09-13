7d861198f29a3892e549196a900e7ad748d84268
henriquemene <henriquemene7@gmail.com>
2022-09-13 09:41:23 -0300

    feito exercicio 1

---
+++ exercicios sequenciais/01-exercicio.py
@@ -0,0 +1,8 @@
+print("-------exercicio 1-------")
+numero1=float(input("escola um numero? "))
+numero2=float(input("agora escolha um outro numero? "))
+if numero1 > numero2:
+    print(f"o primeiro numero escolhido {numero1} é maior  que o segundo {numero2}!")
+
+else :
+    print(f"o segundo numero escolhido {numero2} é maior que o primeiro {numero1}!")
