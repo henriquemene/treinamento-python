052df1164aab28f95e0a872a9126f0d1e8fd89b3
henriquemene <henriquemene7@gmail.com>
2022-09-13 11:19:35 -0300

    feito exercicio 6

---
+++ exercicios sequenciais/06-exercicio.py
@@ -0,0 +1,11 @@
+print("-------exercio 6-------")
+numero1=float(input("escolha um numero ? "))
+numero2=float(input("escolha mais um nuemro ? "))
+numero3=float(input("escolha mais um numero ? "))
+
+if numero1 > numero2 and numero3:
+    print(f"o numero maior dentre os que voce escolheu é o {numero1}")
+if numero2 > numero1 and numero3:
+    print(f"o numero maior dentre os que voce escolheu é o {numero2}")
+if numero3 > numero1 and numero2:
+    print(f"o numero maior dentre os que voce escolheu é o {numero3}")
