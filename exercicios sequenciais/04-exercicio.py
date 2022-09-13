606b3f0d8e2a8d996a3ce9a038c3155c58832b8d
henriquemene <henriquemene7@gmail.com>
2022-09-13 10:19:27 -0300

    HEAD:main

---
+++ exercicios sequenciais/04-exercicio.py
@@ -0,0 +1,6 @@
+print("-------exercicio 4-------")
+letra=str(input("escolha uma letra ?"))
+if letra == "a" "e" "i" "o" "u" "w" "y":
+    print(f"a letra {letra} que voce escolheu e uma vogal ! ")
+else:
+    print(f"a letra {letra} que voce escolheu e uma consoante ! ")
