73fdb816726e6397e305c7098563b2d282706f22
henriquemene <henriquemene7@gmail.com>
2022-09-13 10:40:33 -0300

    feito exercicio 5

---
+++ exercicios sequenciais/05-exercicio.py
@@ -0,0 +1,9 @@
+print("-------excercicio 5-------")
+nota1=float(input("digite a primeira nota "))
+nota2=float(input("digite a segunda nota "))
+media= (nota1 + nota2)/2
+print(f"a media do aluno é {media}")
+if media >= 7:
+    print("PARABENS VOCE FOI APROVADO!")
+else:
+    print("INFELIMENTE VOCE FOI REPROVADO !")
