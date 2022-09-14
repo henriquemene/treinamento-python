603e447746139e9e69ff718fb775302c38a4ff9c
henriquemene <henriquemene7@gmail.com>
2022-09-14 11:15:18 -0300

    feito exercicio 3

--- estrutura de repetição/03-exercicio.py
+++ estrutura de repetição/03-exercicio.py
@@ -1,47 +1,67 @@
 # len
 
 print("-------exercicio 3-------")
-nome=str(input("qual o seu nome ? "))
-idade=float(input("qual é sua idade ? "))
-salario=float(input("qual é o seu salario? "))
-sexo=str(input("qual é o seu sexo ? M ou F ? "))
-civil=str(input("qual é o seu estado civil? s,c,v,d?  "))
 
-qnt_caractere= len(nome)
 
-if qnt_caractere <3:
-    print("digite no minimo 3 caractere ")
-else:
-    print(f"{nome} voce esta cadastrado com sucesso! ")
-    
-    
-if idade >150 :
-    print("digite no maximo 150 e no minimo 0")
-else:
-    print(f"sua idade é {idade} cadastrado com sucesso ! ")
-    
-    
-if salario < 1:
-    print("digite um valor maior que zero ! ")
-else:
-    print(f"seu salario e de {salario}")
-    
-if sexo == "F" :
-    print("voce é feminina")
-if sexo == "M":
-    print("voce e masculino ")
-else:
-    print("porfavor digite F ou M ")
-    
-    
-if civil == "S":
-    print("voce é solteiro")
-if civil == "C":
-    print("voce é casado")
-if civil == "V":
-    print("voce é viuva")
-if civil == "D":
-    print("voce é divorciada ")
-else:
-    print("invalido digite um desses S,C,V,D")
+
+#sexo=str(input("qual é o seu sexo ? M ou F ? "))
+#civil=str(input("qual é o seu estado civil? S,C,V,D?  "))
+
+
+while True:
+    nome=str(input("qual o seu nome ? "))
+    qnt_caractere= len(nome)
+
+    if qnt_caractere >3:
+        print(f"{nome} voce esta cadastrado com sucesso! ")
+        break
+    else:
+        print("digite no minimo 3 caractere ")
+
+
+while True:     
+    idade=float(input("qual é sua idade ? ")) 
+    if idade <150 :
+        print(f"sua idade é {idade} cadastrado com sucesso ! ")
+        break
+    else:
+        print("digite no maximo 150 e no minimo 0")
+     
+while True:
+    salario=float(input("qual é o seu salario? "))  
+    if salario > 1:
+        print(f"seu salario e de {salario} cadastrado com sucesso ! ")
+        break   
+    else:
+        print("digite um valor maior que zero  cadastrado com sucesso ! ")
+
+
+while True: 
+    sexo=str(input("qual é o seu sexo ? M ou F ? "))      
+    if sexo == "F" :
+     print("voce é feminina")
+     break
+    if sexo == "M":
+        print("voce e masculino ")
+        break
+    else:
+      print("porfavor digite F ou M ")
+
+
+while True:   
+    civil=str(input("qual é o seu estado civil? S,C,V,D?  ")) 
+    if civil == "S":
+        print("voce é solteiro")
+        break
+    if civil == "C":
+        print("voce é casado")
+        break
+    if civil == "V":
+        print("voce é viuva")
+        break
+    if civil == "D":
+        print("voce é divorciada ")
+        break
+    else:
+        print("invalido digite um desses S,C,V,D")
     
