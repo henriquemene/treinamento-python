f4415d42507a29cfba683da682c2aa9ceef8525a
henriquemene <henriquemene7@gmail.com>
2022-09-14 09:09:31 -0300

    feito exercicio 1

--- estrutura de repetição/01-exercicio.py
+++ estrutura de repetição/01-exercicio.py
@@ -1,9 +1,9 @@
-
-print("-------exercicio 1-------")
-nota=float(input("escolha uma nota entre 0 e 10 ? "))
-
-if nota >= 0 and nota <= 10:
-    print(f"sua nota foi {nota} ! ")
-else:
-    print("nota invalida")
-    invalido=float(input("digite uma nota novamente ?"))
+while True:
+    print("-------exercicio 1-------")
+    nota=float(input("escolha uma nota entre 0 e 10 "))
+    
+    if nota >= 0 and nota <= 10:
+        print(f"sua nota foi {nota} ! ")
+        break
+    else:
+        print("nota invalida ")
