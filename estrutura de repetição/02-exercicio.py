27cb8687046f92c6057de20b2e5ea75dfdde4be5
henriquemene <henriquemene7@gmail.com>
2022-09-14 10:35:48 -0300

    feito exercicio 2

--- estrutura de repetição/02-exercicio.py
+++ estrutura de repetição/02-exercicio.py
@@ -1,13 +1,13 @@
-print("-------exercicio 2-------")
-nome=str(input("nome de usuario ? "))
-senha=str(input("senha ? "))
+while True:
+    print("-------exercicio 2-------")
+    nome=str(input("nome de usuario ? "))
+    senha=str(input("senha ? "))
 
-if senha == nome :
-    for c in range(5):
+    if senha != nome :
+        print("CADASTRADO COM SUCESSO !")
+        break
+    else:
         print("nao aceitamos usuario e senha igual ! ")
-        nome=str(input("nome de usuario ? "))
-        senha=str(input("senha ? "))
-else:
-    print("login feito com sucesso !")
+       
     
     
