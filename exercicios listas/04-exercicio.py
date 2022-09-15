3b9d62dc6192508a6ad4bbdf79ca26b5e4a95038
henriquemene <henriquemene7@gmail.com>
2022-09-15 11:18:32 -0300

    feito exercicio 4

---
+++ exercicios listas/04-exercicio.py
@@ -0,0 +1,6 @@
+print("------- exercicio 4 -------")
+lista=["r","e","f","e","r","e","n","c","i","a"]
+
+for letra in lista:
+    if letra not in ["a","e","i","o","u"]:
+        print(f"a letra {letra} é uma consoante")
