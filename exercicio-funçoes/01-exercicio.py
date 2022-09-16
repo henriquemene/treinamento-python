cb2d4095cf7af5aca83b0ee88d7b61fe06c65042
henriquemene <henriquemene7@gmail.com>
2022-09-16 15:53:53 -0300

    feito exercicio 1

---
+++ exercicio-funçoes/01-exercicio.py
@@ -0,0 +1,10 @@
+def comando():
+    print(1)
+    print(2,2)
+    print(3,3,3)
+    print(".....")
+    print("N", "N", "N", "N", "N", "N" ,"...", "N")
+
+
+print("-------exercicio 1 -------")
+comando()
