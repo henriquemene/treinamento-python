112124a4c4d9c32ed505218e0a1b037d4be13769
henriquemene <henriquemene7@gmail.com>
2022-09-16 11:16:44 -0300

    feito exercicio 7

---
+++ exercicios listas/07-exercicio.py
@@ -0,0 +1,11 @@
+print("------- exercicio 7 --------")
+
+numero= [5,3,8,1,10]
+soma=sum(numero)
+cont=1
+for x in numero:
+    cont = cont * x
+
+print(f"os numeros escolidos foram {numero}")
+print(f" a soma dos seus numeros sao {soma}")
+print(f"a multiplicaçao dos seus numeros sao {cont}")
