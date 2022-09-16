37aeba688ba327f89a79d2efa4aae206f644684f
henriquemene <henriquemene7@gmail.com>
2022-09-16 15:06:52 -0300

    feito exercicio 9

--- exercicios listas/09-exercicio.py
+++ exercicios listas/09-exercicio.py
@@ -1,8 +1,8 @@
 print("------- exercicio 9 ------")
-A=[4,6,8,2,7,10,24,3,12,3]
+numeros=[4,6,8,2,7,10,24,3,12,3]
 quadrado=1
 
-for C  in A:
+for C  in numeros:
     quadrado = quadrado + (C ** 2)
 
 print(quadrado)
