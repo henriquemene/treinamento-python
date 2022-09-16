d9aacf6ee69fd36d4d88ba01ebc5cac0fc5e1cfe
henriquemene <henriquemene7@gmail.com>
2022-09-16 17:49:11 -0300

    feito exercicio 1

--- exercicio-funçoes/01-exercicio.py
+++ exercicio-funçoes/01-exercicio.py
@@ -1,10 +1,1 @@
-def comando():
-    print(1)
-    print(2,2)
-    print(3,3,3)
-    print(".....")
-    print("N", "N", "N", "N", "N", "N" ,"...", "N")
-
-
-print("-------exercicio 1 -------")
-comando()
+def soma() 
--- exercicios listas/10-exercicio.py
+++ exercicios listas/10-exercicio.py
@@ -1,16 +1,10 @@
+def criar_e_mostrar_triangulo(quantidade_de_linhas):
+    for linha in range(1,quantidade_de_linhas + 1):
+        for coluna in range(linha):
+            print(linha,end=" ")
+        print()
+resposta = int(input("digite um numero : "))
 
 print("------- exercicio 10 -------")
-elemento_a=[]
-elemento_b=[]
-elemento_c=[]
-for c in range(10):
-    elemento_a.append(input(f"digite o elemento da {c +1}+ posicao do primeiro vetor "))
 
-for c in range(10):
-    elemento_b.append(input(f"digite o elemento da {c +1}+ posicao do segundo vetor ")) 
-
-for i in range(10):
-    elemento_c.append(elemento_a[i])
-    elemento_c.append(elemento_b[i])
-
-print(elemento_c)
+criar_e_mostrar_triangulo(resposta)
