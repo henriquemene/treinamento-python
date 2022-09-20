2fb1b66495ce4dc8764e85cce54f851ce217ae4b
henriquemene <henriquemene7@gmail.com>
2022-09-20 13:05:49 -0300

    feito exercicio 1

--- exercicos-strings/01-exercicio.py
+++ exercicos-strings/01-exercicio.py
@@ -2,14 +2,13 @@
 primeira_string = str(input ("digite algo  " ))
 segunda_string = str(input("digite algo novamente  " ))
 
-primeira_quantidade_string=primeira_string
-segunda_quantidade_string=segunda_string
+tamanho_string_1=len(primeira_string)
+tamanho_string_2=len(segunda_string)
 
+print(f"tamanho de {primeira_string} : {tamanho_string_1} caractere " )
+print(f"tamanho de {segunda_string}  : {tamanho_string_2} caractere " )
 
-print(f"tamanho de {primeira_string} : {len(primeira_quantidade_string)} caractere " )
-print(f"tamanho de {segunda_string}  : {len(segunda_quantidade_string)} caractere " )
-
-if primeira_quantidade_string == segunda_quantidade_string:
+if tamanho_string_1 == tamanho_string_2:
     print("as duas strings posuem tamanhos iguais ")
 else:
     print("as duas strings tem tamanho diferentes ")
