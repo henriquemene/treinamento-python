ccaa9f27f265dcd5925a2ac8e2e39aaae0e72111
henriquemene <henriquemene7@gmail.com>
2022-09-21 09:52:08 -0300

    feito exercicio 7

---
+++ exercicos-strings/07-exercicio.py
@@ -0,0 +1,25 @@
+frase_escolhida = input("escolha uma frase: ")
+
+conte_espacos = 0
+conte_vogais = 0
+
+for c in range(0,len(frase_escolhida)):
+    if frase_escolhida[c] == " ":
+        conte_espacos +=  1
+    if frase_escolhida[c] == "a":
+        conte_vogais += 1
+    if frase_escolhida[c] == "e":
+        conte_vogais += 1
+    if frase_escolhida[c] == "i":
+        conte_vogais += 1
+    if frase_escolhida[c] == "o":
+        conte_vogais += 1
+    if frase_escolhida[c] == "u":
+        conte_vogais += 1
+
+
+
+print(f"existe {conte_espacos} espaços em branco em sua frase!" )
+print(f"existe {conte_vogais} vogais em sua frase !" )
+
+
